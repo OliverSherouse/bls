17617837aebc0e8483e9527c35620729b91003d1
Oliver Sherouse <oliver.sherouse@gmail.com>
2017-02-22 16:33:34 -0500

    got rid of unnecessary copyright & marked dev branch

--- bls/__init__.py
+++ bls/__init__.py
@@ -3,20 +3,6 @@ bls.py
 
 A Library to access the Bureau of Labor Statistics API
 """
-#Copyright (C) 2012-2017 Oliver Sherouse <Oliver DOT Sherouse AT gmail DOT com>
-
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program; if not. If not, see <http://www.gnu.org/licenses/>.
 
 from .api import get_series
-__version__ = "0.0.6"
+__version__ = "0.1.0-dev"
