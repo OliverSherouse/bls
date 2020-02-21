7aac596b79b7e47e0231245b4917f21591a1adbd
Oliver Sherouse <oliver@oliversherouse.com>
2020-02-21 14:48:50 -0500

    restored Series tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,12 +14,7 @@ def nokey():
 
 
 def test_monthly_value():
-    assert (
-        bls.get_series("LNS14000000", startyear=1948, endyear=1948)["1948-01"].iloc[0][
-            0
-        ]
-        == 3.4
-    )
+    assert bls.get_series("LNS14000000", startyear=1948, endyear=1948)["1948-01"] == 3.4
 
 
 def test_monthly_value_multiple():
@@ -32,18 +27,14 @@ def test_monthly_value_multiple():
 
 def test_quarterly_value():
     assert (
-        bls.get_series("CIU2020000000000A", startyear=2001, endyear=2001)[
-            "2001-Q1"
-        ].iloc[0][0]
+        bls.get_series("CIU2020000000000A", startyear=2001, endyear=2001)["2001-Q1"]
         == 3.8
     )
 
 
 def test_annual_value():
     assert (
-        bls.get_series("TUU10100AA01000007", startyear=2009, endyear=2009)["2009"].iloc[
-            0
-        ][0]
+        bls.get_series("TUU10100AA01000007", startyear=2009, endyear=2009)["2009"]
         == 148720
     )
 
